153b8b53d67f4f95d6bb22eac2eefc0d16f51c46
willcornforth <141945840+willcornforth@users.noreply.github.com>
2025-05-21 23:07:22 +0100

    Add `rand` dependency and implement random color generation
    This commit introduces the `rand` crate to the project and updates both `Cargo.toml` and `Cargo.lock`. The `setup` function now uses `rand` to generate random RGB values for the `base_color` of meshes, adding variability to the scene's visuals. Additionally, minor code refinements were made for clarity and correctness.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod voxel_world;
 
-use voxel_world::{Chunk, VoxelWorld};
+use voxel_world::{Chunk};
 use bevy::prelude::*;
+use rand::{rng, Rng, RngCore};
 
 fn main() {
   println!("Starting application...");
@@ -16,9 +17,9 @@ fn main() {
 fn setup(
   mut commands: Commands,
   mut materials: ResMut<Assets<StandardMaterial>>,
-  mut meshes: ResMut<Assets<Mesh>>,
+  mut meshes: ResMut<Assets<Mesh>>
 ) {
-  let camera_transform = Transform::from_xyz(50.0, 5.0, 50.0)
+  let camera_transform = Transform::from_xyz(25.0, 5.0, 50.0)
     .looking_at(Vec3::ZERO, Vec3::Y);
 
   // Create a 3D camera.
@@ -27,28 +28,29 @@ fn setup(
     camera_transform
   ));
 
-  // Setup a point light for the scene.
+  // Set up a point light for the scene.
   commands.spawn((PointLight::default(), camera_transform));
 
   let chunk = Chunk::new();
   let chunk_meshes = chunk.generate_mesh();
-  dbg!(&chunk_meshes.len()); // Print mesh size... (Currently, there is one mesh for every voxel.)
 
   // Add the meshes to the scene
-  for mesh in chunk_meshes {
+  for (mesh, position) in chunk_meshes {
     let mesh_handle = meshes.add(mesh);
+
+    let mut rng = rng();
+    let random_r = rng.random_range(0.0..1.0);
+    let random_g = rng.random_range(0.0..1.0);
+    let random_b = rng.random_range(0.0..1.0);
 
     // Create an entity with the mesh
     commands.spawn((
       Mesh3d(mesh_handle),
       MeshMaterial3d(materials.add(StandardMaterial {
-        base_color: Color::linear_rgb(1.0, 1.0, 1.0),
+        base_color: Color::linear_rgb(random_r, random_g, random_b),
         ..default()
-      }))
+      })),
+      Transform::from_translation(position),
     ));
   }
-}
-
-fn create_cube_mesh() -> Mesh {
-  Mesh::from(Cuboid::new(1.0, 1.0, 1.0))
 }
